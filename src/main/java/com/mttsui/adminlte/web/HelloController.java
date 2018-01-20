f47fa545769e2365d7b8f9da92b60bb89c3c2a98
mttsui <xzs603@gmail.com>
2018-01-20 15:49:28 +0800

    Initial commit

---
+++ src/main/java/com/mttsui/adminlte/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.mttsui.adminlte.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("")
+    public String index(){
+        return "index";
+    }
+}
